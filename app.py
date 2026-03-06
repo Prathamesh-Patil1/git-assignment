d20ace07ebd0c5083d8f2221f2d4ac50e24fb923
Prathamesh-Patil1 <patilprathamesh9777@gmail.com>
2026-03-06 19:37:42 +0530

    Rollback to Item ID state using soft reset

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import json
 import os
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
 # Path to the data.json file
 DATA_FILE = os.path.join(os.path.dirname(__file__), "data.json")
+
+# MongoDB connection
+client = MongoClient("mongodb://localhost:27017/")
+db = client["todo_db"]
+collection = db["todo_items"]
 
 # Home route
 @app.route("/")
@@ -22,7 +28,23 @@ def get_data():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+
+# NEW ROUTE FOR TASK 3
+@app.route("/submittodoitem", methods=["POST"])
+def submit_todo():
+
+    itemName = request.form.get("itemName")
+    itemDescription = request.form.get("itemDescription")
+
+    todo_item = {
+        "itemName": itemName,
+        "itemDescription": itemDescription
+    }
+
+    collection.insert_one(todo_item)
+
+    return jsonify({"message": "Todo item saved successfully"})
+
+
 if __name__ == "__main__":
     app.run(debug=True)
-
-#ppatil2004
